4950ac8300f8efc25741fb87c879d6b5b8afc567
minfawang <minfa.wang@gmail.com>
2016-05-23 00:18:33 -0700

    Define nest schema in Home

--- src/Home/client/components/Home.jsx
+++ src/Home/client/components/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+const Location = new SimpleSchema({
+  address: { type: String },
+  city: { type: String },
+});
+
+const schema = new SimpleSchema({
+  name: { type: String },
+  email: { type: String },
+  location: { type: Location },
+});
 
 class TodoApp extends React.Component {
   render() {
+    global.schema = schema;
+
     return (
       <h1>Home Page of Experiments</h1>
     );
